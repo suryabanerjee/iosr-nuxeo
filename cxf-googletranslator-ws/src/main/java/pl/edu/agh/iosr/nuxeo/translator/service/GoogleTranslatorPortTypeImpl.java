889e1ca67b78ae639bb367b5d6b543f574e84e8c
lewickitom <lewickitom@gmail.com>
2010-01-02 11:18:56 +0000

    new management of supported languages

--- cxf-googletranslator-ws/src/main/java/pl/edu/agh/iosr/nuxeo/translator/service/GoogleTranslatorPortTypeImpl.java
+++ cxf-googletranslator-ws/src/main/java/pl/edu/agh/iosr/nuxeo/translator/service/GoogleTranslatorPortTypeImpl.java
@@ -40,45 +40,17 @@ import pl.edu.agh.iosr.nuxeo.wsdl.translator.TranslatorPortType;
         endpointInterface="pl.edu.agh.iosr.nuxeo.wsdl.translator.TranslatorPortType")
         
 public class GoogleTranslatorPortTypeImpl implements TranslatorPortType{
-//		
+//TODO faulty
 
 	private Operation[] supportedOperations={Operation.TRANSLATION,Operation.LANGUAGE_DETECTION};
 	private SourceType[] supportedSourceTypes={SourceType.STRING};
 	private FileFormat[] supportedSourceFileFormats={FileFormat.PLAIN_TEXT};
-	private List<LanguagePair> supportedLangugagePairs;
-	private List<String> detectionSupportedLangugages;
 	
+	private SupportedLanguagesManager supportedLanguagesManager=new SupportedLanguagesManager();
 	
-	private List<LanguagePair> getSupportedLangugagePairsList(){
-		
-		List<LanguagePair> pairList=new ArrayList<LanguagePair>();
-		for(Language l1:Language.values()){
-			for(Language l2:Language.values()){
-				if(l1!=Language.AUTO_DETECT){
-					if(l1!=l2 && l2!=Language.AUTO_DETECT){
-						LanguagePair pair=new LanguagePair();
-						pair.setSrcLanguage(l1.toString());
-						pair.setDestLanguage(l2.toString());
-						pairList.add(pair);
-					}
-				}
-			}
-		}
-		return pairList;
-	}
-	
-	private List<String> getDetectionSupportedLangugagesList(){
-		List<String> pairList=new ArrayList<String>();
-		for(Language l:Language.values()){
-			if(l!=Language.AUTO_DETECT)
-				pairList.add(l.toString());
-		}
-		return pairList;
-		
-	}
-	
+
 	@Override
-	public String translate(TranslationRequest parameter) {
+	public String translate(TranslationRequest parameter) {		//TODO splitting, to result and void instead of String
 		Translate.setHttpReferrer("localhost");		
 		if(parameter.getContentSource().getSourceType()==SourceType.STRING){
 			StringContentSource content=(StringContentSource)parameter.getContentSource();
@@ -140,59 +112,24 @@ public class GoogleTranslatorPortTypeImpl implements TranslatorPortType{
 	
 	@Override
 	public Languages getSupportedDetections(Object parameter) {
-		Languages languages=new Languages();
-		
-	/*	languages.getLanguages().add(Locale.ENGLISH.toString());
-		languages.getLanguages().add(Locale.GERMAN.toString());*/
-		if(detectionSupportedLangugages==null){
-			detectionSupportedLangugages=getDetectionSupportedLangugagesList();
-		}
-		languages.getLanguages().addAll(detectionSupportedLangugages);
-		return languages;
+
+		return supportedLanguagesManager.getDetectionSupportedLangugagesList();
+
 	}
 	
 
 
 	@Override
 	public LanguagePairs getSupportedTranslations(Object parameter) {
-		/*
-		LanguagePairs pairs=new LanguagePairs();
-		LanguagePair l1=new LanguagePair();
-		LanguagePair l2=new LanguagePair();
-		l1.setSrcLanguage(Locale.ENGLISH.toString());
-		l1.setDestLanguage(Locale.GERMAN.toString());
-		l1.setSrcLanguage(Locale.GERMAN.toString());
-		l2.setDestLanguage(Locale.ENGLISH.toString());
-		pairs.getLanguagePairs().add(l1);
-		pairs.getLanguagePairs().add(l2);
-		*/
-		LanguagePairs langs=new LanguagePairs();
-		if(supportedLangugagePairs==null){
-			supportedLangugagePairs=getSupportedLangugagePairsList();
-		}
-		langs.getLanguagePairs().addAll(supportedLangugagePairs);
-		return langs;
+
+		return supportedLanguagesManager.getSupportedLangugagePairsList();
+
 	}
-
-
 
 	@Override
 	public TranslationQualities getSupportedQualities(Object parameter) {
-/*		TranslationQualities qualities=new TranslationQualities();
-		TranslationQuality quality1=new TranslationQuality ();
-		TranslationQuality quality2=new TranslationQuality ();
-		TranslationQuality quality3=new TranslationQuality ();
-		quality1.setQualityName("LOW");
-		quality1.setQualityName("MEDIUM");
-		quality1.setQualityName("HIGH");
-		qualities.getTranslationQualities().add(quality1);
-		qualities.getTranslationQualities().add(quality2);
-		qualities.getTranslationQualities().add(quality3);*/
-		
-		TranslationQualities qualities=new TranslationQualities();
-		return qualities;
+		return null;
 	}
-
 
 	@Override
 	public boolean ping(Object parameter) {
---
+++ cxf-googletranslator-ws/src/main/java/pl/edu/agh/iosr/nuxeo/translator/service/SupportedLanguagesManager.java
@@ -0,0 +1,51 @@
+package pl.edu.agh.iosr.nuxeo.translator.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pl.edu.agh.iosr.nuxeo.schema.translator.LanguagePair;
+import pl.edu.agh.iosr.nuxeo.schema.translator.LanguagePairs;
+import pl.edu.agh.iosr.nuxeo.schema.translator.Languages;
+
+import com.google.api.translate.Language;
+
+public class SupportedLanguagesManager {
+
+
+	
+	public LanguagePairs getSupportedLangugagePairsList(){
+		
+		List<LanguagePair> pairList=new ArrayList<LanguagePair>();
+			for(Language l1:Language.values()){
+				for(Language l2:Language.values()){
+					if(l1!=Language.AUTO_DETECT){
+						if(l1!=l2 && l2!=Language.AUTO_DETECT){
+							LanguagePair pair=new LanguagePair();
+							pair.setSrcLanguage(l1.toString());
+							pair.setDestLanguage(l2.toString());
+							pairList.add(pair);
+						}
+					}
+				}
+		}
+		LanguagePairs languagePairs=new LanguagePairs();
+		languagePairs.getLanguagePairs().addAll(pairList);
+		return languagePairs;
+	}
+	
+	public Languages getDetectionSupportedLangugagesList(){
+		
+		List<String> pairList=new ArrayList<String>();
+		for(Language l:Language.values()){
+			if(l!=Language.AUTO_DETECT)
+				pairList.add(l.toString());
+		}
+			
+		Languages languages=new Languages();
+		languages.getLanguages().addAll(pairList);
+		return languages;
+		
+	}
+	
+	
+}
